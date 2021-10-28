63173e0b0e29a6ef7038b6477dea315991ee529c
nipkow <skidrowa@gmail.com>
2021-10-28 00:40:08 -0300

    Functions to increase memory limits and video res

--- dllmain/dllmain.cpp
+++ dllmain/dllmain.cpp
@@ -6,8 +6,10 @@
 std::string RealDllPath;
 std::string WrapperMode;
 std::string WrapperName;
+std::string game_version;
 std::string QTE_key_1;
 std::string QTE_key_2;
+std::string SFD_DisplayResolution;
 std::string flip_item_up;
 std::string flip_item_down;
 std::string flip_item_left;
@@ -48,6 +50,7 @@ bool bRestorePickupTransparency;
 bool bDisableFXAA;
 bool bDisableFilmGrain;
 bool bIgnoreFPSWarning;
+bool bMemOptimi;
 
 uintptr_t jmpAddrQTE1icon1;
 uintptr_t jmpAddrQTE1icon2;
@@ -68,6 +71,29 @@ static uint32_t* ptrIsColorFilter;
 static uint32_t* ptrMovState;
 static uint32_t* ptrEngineWidthScale;
 static uint32_t* ptrAspectRatio;
+
+static uint32_t* ptrcSofdec__startApp;
+static uint32_t* ptrmwPlyCalcWorkCprmSfd;
+static uint32_t* ptrcSofdec__finishMovie;
+static uint32_t* ptrMemPoolMovie;
+
+static uint32_t* ptrpG;
+static uint32_t* ptrpzzl_size;
+static uint32_t* ptrstageInit;
+static uint32_t* ptrSubScreenAramRead;
+static uint32_t* ptrp_MemPool_SubScreen1;
+static uint32_t* ptrp_MemPool_SubScreen2;
+static uint32_t* ptrp_MemPool_SubScreen3;
+static uint32_t* ptrp_MemPool_SubScreen4;
+static uint32_t* ptrp_MemPool_SubScreen5;
+static uint32_t* ptrp_MemPool_SubScreen6;
+static uint32_t* ptrp_MemPool_SubScreen7;
+static uint32_t* ptrp_MemPool_SubScreen8;
+static uint32_t* ptrp_MemPool_SubScreen9;
+static uint32_t* ptrg_MemPool_SubScreen1;
+static uint32_t* ptrg_MemPool_SubScreen2;
+static uint32_t* ptrg_MemPool_SubScreen3;
+static uint32_t* ptrg_MemPool_SubScreen4;
 
 uint32_t intGameWidth;
 uint32_t intGameHeight;
@@ -548,6 +574,88 @@ void __declspec(naked) LoadPos_RadioNames()
 	}
 }
 
+// SFD functions
+typedef int(__cdecl* mwPlyCalcWorkCprmSfd_Fn)(/* MwsfdCrePrm * */ void* cprm);
+mwPlyCalcWorkCprmSfd_Fn mwPlyCalcWorkCprmSfd_Orig;
+int __cdecl mwPlyCalcWorkCprmSfd_Hook(/* MwsfdCrePrm * */ void* cprm)
+{
+	int workarea_size = mwPlyCalcWorkCprmSfd_Orig(cprm);
+
+	// alloc a workarea based on what mwPlyCalcWorkCprmSfd_Orig returned - allows high-res videos to play without crashing :)
+	void* workarea = malloc(workarea_size);
+
+	void** g_MemPoolMovie = (void**)(ptrMemPoolMovie);
+	*g_MemPoolMovie = workarea;
+
+	return workarea_size;
+}
+
+typedef void(__fastcall* cSofdec__startApp_Fn)(void* thisptr);
+cSofdec__startApp_Fn cSofdec__startApp_Orig;
+
+void __fastcall cSofdec__startApp_Hook(void* thisptr)
+{
+	// Backup original g_MemPoolMovie value
+	// we need to change this so that cSofdec::startApp uses our own memory block instead of the hardcoded (& incorrectly-sized) memory pool
+	// a few other things rely on g_MemPoolMovie value though, so it needs to be changed back after this func has ran
+	// unfortunately we dont know the size of g_MemPoolMovie until mwPlyCalcWorkCprmSfd is called though (during cSofdec::startApp)
+	// so a mwPlyCalcWorkCprmSfd hook is used to handle allocating & updating g_MemPoolMovie, while cSofdec::startApp hook is just used to backup/restore original addr
+
+	void** g_MemPoolMovie = (void**)(ptrMemPoolMovie);
+	void* g_MemPoolMovie_Original = *g_MemPoolMovie;
+
+	cSofdec__startApp_Orig(thisptr);
+
+	*g_MemPoolMovie = g_MemPoolMovie_Original;
+}
+
+typedef void(__fastcall* cSofdec__finishMovie_Fn)(uint8_t* thisptr);
+cSofdec__finishMovie_Fn cSofdec__finishMovie_Orig;
+void __fastcall cSofdec__finishMovie_Hook(uint8_t* thisptr)
+{
+	cSofdec__finishMovie_Orig(thisptr);
+
+	// cleanup the workarea we allocated
+	// ps2 version seems to memset(0) the workarea here, PC version doesn't do this though
+	// should mean it's safe to assume the workarea isn't being used anymore, so we can free it here
+	void* workarea = *(void**)(thisptr + 0x130);
+	free(workarea);
+}
+
+// Inventory screen mem functions
+uint8_t g_MemPool_SubScreen[0x4000000];
+uint8_t g_MemPool_SubScreen2[0x4000000];
+
+uint8_t* p_MemPool_SubScreen = (uint8_t*)&g_MemPool_SubScreen;
+
+typedef bool(__fastcall* MessageControl__stageInit_Fn)(void* thisptr);
+MessageControl__stageInit_Fn MessageControl__stageInit_Orig;
+
+bool __fastcall MessageControl__stageInit_Hook(void* thisptr)
+{
+	bool ret = MessageControl__stageInit_Orig(thisptr);
+
+	// Set pG+0x3C to point to an extended buffer we control
+	// The game uses this for some MemorySwap operation later on - not really sure why that swap is needed though, but oh well
+	uint8_t* pG = *(uint8_t**)(ptrpG);
+
+	*(void**)(pG + 0x3C) = &g_MemPool_SubScreen2;
+
+	return ret;
+}
+
+typedef int(*SubScreenAramRead_Fn)();
+SubScreenAramRead_Fn SubScreenAramRead_Orig;
+int SubScreenAramRead_Hook()
+{
+	int pzzl_size = SubScreenAramRead_Orig();
+
+	// Patch SubScreenExec heap size, seems to be max size to read from ss_pzzl/omk_pzzl file
+	injector::WriteMemory<int>(ptrpzzl_size, pzzl_size, true);
+
+	return pzzl_size;
+}
+
 void ReadSettings()
 {
 	CIniReader iniReader("");
@@ -565,6 +673,8 @@ void ReadSettings()
 	QTE_key_1 = iniReader.ReadString("KEYBOARD", "QTE_key_1", "D");
 	QTE_key_2 = iniReader.ReadString("KEYBOARD", "QTE_key_2", "A");
 	bIgnoreFPSWarning = iniReader.ReadBoolean("FRAME RATE", "IgnoreFPSWarning", false);
+	SFD_DisplayResolution = iniReader.ReadString("MOVIE", "SFD_DisplayResolution", "512x336");
+	bMemOptimi = iniReader.ReadBoolean("EXPERIMENTAL", "MemOptimi", false);
 }
 
 void HandleAppID()
@@ -609,10 +719,6 @@ void GetPointers()
 	// Is inventory open
 	ptrIsInventoryOpen = injector::ReadMemory<int>(StateBase, true) + 0x58;
 
-	// SFD player status
-	pattern = hook::pattern("89 3D ? ? ? ? E8 ? ? ? ? 8B ? ? 51 E8 ? ? ? ? A1 ? ? ? ? 83 ? ? 2B C7 89");
-	ptrMovState = *pattern.count(1).get(0).get<uint32_t*>(2);
-
 	// Is motion blur enabled
 	pattern = hook::pattern("80 3D ? ? ? ? ? 74 ? 84 DB 75 ? 8B 0D ? ? ? ? 8B 91 ? ? ? ? C1 EA");
 	ptrIsMotionBlur = *pattern.count(1).get(0).get<uint32_t*>(2), true;
@@ -620,17 +726,161 @@ void GetPointers()
 	// Is color filter enabled
 	pattern = hook::pattern("A3 ? ? ? ? 5D C3 CC CC CC A1 ? ? ? ? C3 CC CC CC CC CC CC CC CC CC CC 55 8B EC 8A 45");
 	ptrIsColorFilter = *pattern.count(1).get(0).get<uint32_t*>(1), true;
+
+	// SFD
+	pattern = hook::pattern("89 3D ? ? ? ? E8 ? ? ? ? 8B ? ? 51 E8 ? ? ? ? A1 ? ? ? ? 83 ? ? 2B C7 89");
+	ptrMovState = *pattern.count(1).get(0).get<uint32_t*>(2);
+
+	pattern = hook::pattern("56 57 8B F9 6A ? 8D 77 ? 6A ? 56 E8 ? ? ? ? C7 46 ? ? ? ? ? C7 06");
+	ptrcSofdec__startApp = pattern.count(1).get(0).get<uint32_t>(0);
+
+	pattern = hook::pattern("56 8B 74 24 ? 85 F6 75 ? 68 ? ? ? ? E8 ? ? ? ? 83 C4 ? 33 C0 5E C3 8B 06");
+	ptrmwPlyCalcWorkCprmSfd = pattern.count(1).get(0).get<uint32_t>(0);
+
+	pattern = hook::pattern("56 8B F1 57 8D 86 ? ? ? ? 50 33 FF 68 ? ? ? ? 89 3D ? ? ? ? E8 ? ? ? ? 8B 4E");
+	ptrcSofdec__finishMovie = pattern.count(1).get(0).get<uint32_t>(0);
+
+	pattern = hook::pattern("8B 15 ? ? ? ? 81 C2 ? ? ? ? 6A ? 51 89 56 ? E8 ? ? ? ? 8B 4E");
+	ptrMemPoolMovie = injector::ReadMemory<uint32_t*>(pattern.count(1).get(0).get<uint32_t*>(2), true);
+
+	// Inventory screen mem
+	pattern = hook::pattern("A1 ? ? ? ? 56 D9 58 ? FF 15 ? ? ? ? 50 FF D7 8B 8D ? ? ? ? 8B 95 ? ? ? ? 89 0D");
+	ptrpG = injector::ReadMemory<uint32_t*>(pattern.count(1).get(0).get<uint32_t*>(1), true);
+
+	pattern = hook::pattern("05 ? ? ? ? F6 C1 ? 8B 0D ? ? ? ? A3 ? ? ? ? 8D 91 ? ? ? ? 75 ? 8D 91 ? ? ? ? 52 03 C1");
+	ptrpzzl_size = pattern.count(1).get(0).get<uint32_t>(1);
+
+	pattern = hook::pattern("55 8B EC 81 EC ? ? ? ? A1 ? ? ? ? 33 C5 89 45 ? A1 ? ? ? ? 8A 80");
+	ptrstageInit = pattern.count(1).get(0).get<uint32_t>(0);
+
+	pattern = hook::pattern("55 8B EC 83 EC ? A1 ? ? ? ? 56 68 ? ? ? ? 68 ? ? ? ? 6A ? 6A ? 6A ? 05");
+	ptrSubScreenAramRead = pattern.count(1).get(0).get<uint32_t>(0);
+
+	pattern = hook::pattern("8B 0D ? ? ? ? 03 F8 81 C1 ? ? ? ? 3B F9 73 ? 89 46 ? EB ? 81 FF ? ? ? ? 73 ? 8B 15");
+	ptrp_MemPool_SubScreen1 = pattern.count(1).get(0).get<uint32_t>(2);
+
+	pattern = hook::pattern("8B 15 ? ? ? ? 81 C2 ? ? ? ? 89 56 ? 83 0E ? 8B 06 A8 ? 74 ? 8B 4E ? 8B 56 ? 8B 45");
+	ptrp_MemPool_SubScreen2 = pattern.count(1).get(0).get<uint32_t>(2);
+
+	pattern = hook::pattern("A1 ? ? ? ? 05 ? ? ? ? 39 46 ? 72 ? 6A ? E8 ? ? ? ? 83 C4 ? 8B 4E ? 51");
+	ptrp_MemPool_SubScreen3 = pattern.count(1).get(0).get<uint32_t>(1);
+
+	pattern = hook::pattern("08 A1 ? ? ? ? 56 68 ? ? ? ? 68 ? ? ? ? 6A ? 6A ? 6A ? 05 ? ? ? ? 50 6A ? 68");
+	ptrp_MemPool_SubScreen4 = pattern.count(1).get(0).get<uint32_t>(2);
+
+	pattern = hook::pattern("8B 0D ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 6A ? 6A ? 6A ? 8D 94 ? ? ? ? ? 52 6A");
+	if (game_version == "1.0.6")
+		ptrp_MemPool_SubScreen5 = pattern.count(1).get(0).get<uint32_t>(2);
+	else
+		ptrp_MemPool_SubScreen5 = pattern.count(1).get(0).get<uint32_t>(-4);
+
+	pattern = hook::pattern("8B 15 ? ? ? ? A1 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 6A ? 6A ? 6A ? 8D 8C");
+	if (game_version == "1.0.6")
+		ptrp_MemPool_SubScreen6 = pattern.count(1).get(0).get<uint32_t>(2);
+	else
+		ptrp_MemPool_SubScreen6 = pattern.count(1).get(0).get<uint32_t>(7);
+
+	pattern = hook::pattern("8B 0D ? ? ? ? A1 ? ? ? ? 8B 40 ? 68 ? ? ? ? 81 C1 ? ? ? ? 51 50 A3");
+	ptrp_MemPool_SubScreen7 = pattern.count(1).get(0).get<uint32_t>(2);
+
+	pattern = hook::pattern("15 ? ? ? ? 8B 86 ? ? ? ? 68 ? ? ? ? 81 C2 ? ? ? ? 52 50 E8 ? ? ? ? 6A");
+	ptrp_MemPool_SubScreen8 = pattern.count(1).get(0).get<uint32_t>(1);
+
+	pattern = hook::pattern("8B 0D ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 6A ? 53 53 8D 94 08 ? ? ? ? 52 53");
+	ptrp_MemPool_SubScreen9 = pattern.count(1).get(0).get<uint32_t>(2);
+
+	pattern = hook::pattern("68 ? ? ? ? 81 C1 ? ? ? ? 51 50 A3 ? ? ? ? 88 1D ? ? ? ? E8");
+	ptrg_MemPool_SubScreen1 = pattern.count(1).get(0).get<uint32_t>(1);
+
+	pattern = hook::pattern("68 ? ? ? ? 81 C2 ? ? ? ? 52 50 E8 ? ? ? ? 6A ? E8 ? ? ? ? 6A ? E8 ");
+	ptrg_MemPool_SubScreen2 = pattern.count(1).get(0).get<uint32_t>(1);
+
+	pattern = hook::pattern("8D 91 ? ? ? ? 75 ? 8D 91 ? ? ? ? 52 03 C1 50 6A ? E8 ? ? ? ? 83");
+	ptrg_MemPool_SubScreen3 = pattern.count(1).get(0).get<uint32_t>(2);
+	ptrg_MemPool_SubScreen4 = pattern.count(1).get(0).get<uint32_t>(10);
 }
 
-void Init()
+void HandleLimits()
+{
+	// vertex buffers
+	auto pattern = hook::pattern("68 80 1A 06 00 50 8B 41 ? FF D0 85 C0 0F 85 ? ? ? ? 46");
+	injector::WriteMemory<int>(pattern.count(2).get(0).get<uint32_t>(1), 0x68000C3500, true);  // 400000 -> 800000
+	injector::WriteMemory<int>(pattern.count(2).get(1).get<uint32_t>(1), 0x68000C3500, true);
+
+	pattern = hook::pattern("68 00 09 3D 00 50 8B 41 ? FF D0 85 C0 0F 85 ? ? ? ? A1 ? ? ? ? 8B 08");
+	injector::WriteMemory<int>(pattern.count(2).get(0).get<uint32_t>(1), 0x68007A1200, true);  // 4000000 -> 8000000
+	injector::WriteMemory<int>(pattern.count(2).get(1).get<uint32_t>(1), 0x68007A1200, true);
+
+	pattern = hook::pattern("68 80 84 1E 00 50 8B 41 ? FF D0 85 C0 0F 85 ? ? ? ? A1");
+	injector::WriteMemory<int>(pattern.count(2).get(0).get<uint32_t>(1), 0x68003D0900, true);  // 2000000 -> 4000000
+	injector::WriteMemory<int>(pattern.count(2).get(1).get<uint32_t>(1), 0x68003D0900, true);
+
+	pattern = hook::pattern("68 80 8D 5B 00 50 8B 41 ? FF D0 85 C0 0F 85");
+	injector::WriteMemory<int>(pattern.count(4).get(0).get<uint32_t>(1), 0x6800B71B00, true);  // 6000000 -> 12000000
+	injector::WriteMemory<int>(pattern.count(4).get(1).get<uint32_t>(1), 0x6800B71B00, true);
+	injector::WriteMemory<int>(pattern.count(4).get(2).get<uint32_t>(1), 0x6800B71B00, true);
+	injector::WriteMemory<int>(pattern.count(4).get(3).get<uint32_t>(1), 0x6800B71B00, true);
+
+	// Inventory screen mem
+	MH_CreateHook(ptrstageInit, MessageControl__stageInit_Hook, (LPVOID*)&MessageControl__stageInit_Orig);
+	MH_CreateHook(ptrSubScreenAramRead, SubScreenAramRead_Hook, (LPVOID*)&SubScreenAramRead_Orig);
+
+	p_MemPool_SubScreen = (uint8_t*)&g_MemPool_SubScreen;
+
+	injector::WriteMemory<int>(ptrp_MemPool_SubScreen1, (uintptr_t)&p_MemPool_SubScreen, true);
+	injector::WriteMemory<int>(ptrp_MemPool_SubScreen2, (uintptr_t)&p_MemPool_SubScreen, true);
+	injector::WriteMemory<int>(ptrp_MemPool_SubScreen3, (uintptr_t)&p_MemPool_SubScreen, true);
+
+	injector::WriteMemory<int>(ptrp_MemPool_SubScreen4, (uintptr_t)&p_MemPool_SubScreen, true); // SubScreenAramRead, reads rel/Sscrn.rel
+	injector::WriteMemory<int>(ptrp_MemPool_SubScreen5, (uintptr_t)&p_MemPool_SubScreen, true); // SubScreenAramRead, reads  ss_cmmn.dat read
+	injector::WriteMemory<int>(ptrp_MemPool_SubScreen6, (uintptr_t)&p_MemPool_SubScreen, true); // SubScreenAramRead, reads  omk_pzzl.dat / ss_pzzl.dat
+
+	injector::WriteMemory<int>(ptrp_MemPool_SubScreen7, (uintptr_t)&p_MemPool_SubScreen, true); // SubScreenExec MemorySwap call
+	injector::WriteMemory<int>(ptrp_MemPool_SubScreen8, (uintptr_t)&p_MemPool_SubScreen, true); // SubScreenExitCore MemorySwap call
+	injector::WriteMemory<int>(ptrp_MemPool_SubScreen9, (uintptr_t)&p_MemPool_SubScreen, true); // SubScreenExit
+
+	#define SS_MEM_OFFSET 0x1CB5400 // SubScreenAramRead adds this offset to the memory addr, could probably be patched out, but meh
+	injector::WriteMemory<int>(ptrg_MemPool_SubScreen1, (uint32_t)(sizeof(g_MemPool_SubScreen) - SS_MEM_OFFSET), true); // SubScreenExec MemorySwap size
+	injector::WriteMemory<int>(ptrg_MemPool_SubScreen2, (uint32_t)(sizeof(g_MemPool_SubScreen) - SS_MEM_OFFSET), true); // SubScreenExitCore MemorySwap size
+
+	injector::WriteMemory<int>(ptrg_MemPool_SubScreen3, (uint32_t)(sizeof(g_MemPool_SubScreen) - 0x1000000), true); // SubScreenExec, some heap size
+	injector::WriteMemory<int>(ptrg_MemPool_SubScreen4, (uint32_t)(sizeof(g_MemPool_SubScreen) - 0x2000000), true); // SubScreenExec, some heap size
+}
+
+bool Init()
 {
 	std::cout << "Big ironic thanks to QLOC S.A." << std::endl;
+
+	// Detect game version
+	auto pattern = hook::pattern("31 2E ? ? ? 00 00 00 6D 6F 76 69 65 2F 64 65 6D 6F 30 65 6E 67 2E 73 66 64");
+	int ver = injector::ReadMemory<int>(pattern.get_first(2));
+
+	std::cout << std::hex << pattern.get_first(0) << std::endl;
+
+	std::cout << std::hex << ver << std::endl;
+
+	if (ver == 0x362E30) {
+		game_version = "1.0.6";
+	} else if (ver == 0x302E31) {
+		game_version = "1.1.0";
+	} else {
+		MessageBoxA(NULL, "This version of RE4 is not supported.\nre4_tweaks will be disabled.", "re4_tweaks", MB_ICONERROR | MB_SYSTEMMODAL | MB_SETFOREGROUND);
+		return false;
+	}
+
+	#ifdef VERBOSE
+	std::cout << "Game version = " << game_version << std::endl;
+	#endif
+
+	MH_Initialize();
 
 	ReadSettings();
 
 	HandleAppID();
 
 	GetPointers();
+
+	HandleLimits();
 
 	// FOV
 	if (fFOVAdditional != 0.0f)
@@ -640,6 +890,60 @@ void Init()
 		injector::MakeCALL(pattern.get_first(0), ScaleFOV, true);
 	}
 	
+	// SFD size
+	if (SFD_DisplayResolution != "512x336")
+	{
+		// Create mem hooks
+		MH_CreateHook(ptrcSofdec__startApp, cSofdec__startApp_Hook, (LPVOID*)&cSofdec__startApp_Orig);
+		MH_CreateHook(ptrmwPlyCalcWorkCprmSfd, mwPlyCalcWorkCprmSfd_Hook, (LPVOID*)&mwPlyCalcWorkCprmSfd_Orig);
+		MH_CreateHook(ptrcSofdec__finishMovie, cSofdec__finishMovie_Hook, (LPVOID*)&cSofdec__finishMovie_Orig);
+
+		// Parse resolution using an array
+		char* ResString[] = {SFD_DisplayResolution.data()};
+		char* ResArray[2], *next_token;
+
+		int i = 0;
+		ResArray[i] = strtok_s(*ResString, "x", &next_token);
+
+		while (ResArray[i] != NULL)
+		{
+			ResArray[++i] = strtok_s(NULL, "/", &next_token);
+		}
+
+		int MovPosX;
+		int MovPosY;
+		assert(sscanf_s(ResArray[0], "%d", &MovPosX) > 0);
+		assert(sscanf_s(ResArray[1], "%d", &MovPosY) > 0);
+
+		// Calculate new resolution
+		int newMovPosX = MovPosX / 2;
+		int newMovNegX = -newMovPosX;
+
+		int newMovPosY = MovPosY / 1.5;
+		int newMovNegY = -newMovPosY;
+
+		#ifdef VERBOSE
+		std::cout << "newMovPosX = " << newMovPosX << std::endl;
+		std::cout << "newMovNegX = " << newMovNegX << std::endl;
+
+		std::cout << "newMovPosY = " << newMovPosY << std::endl;
+		std::cout << "newMovNegY = " << newMovNegY << std::endl;
+		#endif
+
+		auto pattern = hook::pattern("D9 05 ? ? ? ? D9 5C 24 ? D9 05 ? ? ? ? D9 5C 24 ? D9 05 ? ? ? ? D9 5C 24 ? D9 05 ? ? ? ? D9 1C ? 50 E8 ? ? ? ? 8D 4D ? 6A ? 51 E8 ? ? ? ? 8D 55 ? 52 8D 45 ? 50 8D 4D ? 51 56 E8 ? ? ? ? 8B 4D ? 33 CD");
+		static uint32_t* ptrMovPosX = injector::ReadMemory<uint32_t*>(pattern.count(1).get(0).get<uint32_t*>(2), true);
+		static uint32_t* ptrMovNegX = injector::ReadMemory<uint32_t*>(pattern.count(1).get(0).get<uint32_t*>(12), true);
+
+		static uint32_t* ptrMovPosY = injector::ReadMemory<uint32_t*>(pattern.count(1).get(0).get<uint32_t*>(32), true);
+		static uint32_t* ptrMovNegY = injector::ReadMemory<uint32_t*>(pattern.count(1).get(0).get<uint32_t*>(22), true);
+
+		// Write new values
+		injector::WriteMemory<float>(ptrMovPosX, newMovPosX, true);
+		injector::WriteMemory<float>(ptrMovNegX, newMovNegX, true);
+		injector::WriteMemory<float>(ptrMovPosY, newMovPosY, true);
+		injector::WriteMemory<float>(ptrMovNegY, newMovNegY, true);
+	}
+
 	// Check if the game is running at a valid frame rate
 	if (!bIgnoreFPSWarning)
 	{
@@ -647,6 +951,15 @@ void Init()
 		ptrGameFrameRate = *pattern.count(1).get(0).get<uint32_t*>(2);
 		injector::MakeNOP(pattern.get_first(0), 6, true);
 		injector::MakeCALL(pattern.get_first(0), ReadFPS, true);
+	}
+
+	// Replace MemorySwap with memcpy
+	if (bMemOptimi)
+	{
+		//MH_CreateHook((LPVOID)(GameAddress + 0x724D), memcpy, NULL);
+		//std::cout << std::hex << GameAddress + 0x724D << std::endl;
+		auto pattern = hook::pattern("8b 49 14 33 c0 a3 ? ? ? ? 85 c9 74 ? 8b ff f6 81 ? ? ? ? 20 8b 49 ? 74 ? b8 01 00 00 00 a3 ? ? ? ? 85 c9 75 ? c3");
+		injector::MakeJMP(pattern.get_first(0), memcpy, true);
 	}
 
 	// Fix QTE mashing speed issues
@@ -804,7 +1117,7 @@ void Init()
 	// QTE bindings and icons
 	// KEY_1 binding hook
 	intQTE_key_1 = getMapKey(QTE_key_1, "dik");
-	auto pattern = hook::pattern("8B 58 ? 8B 40 ? 8D 8D ? ? ? ? 51");
+	pattern = hook::pattern("8B 58 ? 8B 40 ? 8D 8D ? ? ? ? 51");
 	injector::MakeNOP(pattern.get_first(0), 6, true);
 	injector::MakeCALL(pattern.get_first(0), QTE1Binding, true);
 
@@ -861,6 +1174,11 @@ void Init()
 	
 	injector::MakeNOP(pattern.get_first(9), 7, true);
 	injector::MakeCALL(pattern.get_first(9), invItemUp, true);
+
+	// Enable MH hooks
+	MH_EnableHook(MH_ALL_HOOKS);
+
+	return true;
 }
 
 DWORD WINAPI ContinuousThread(LPVOID)
@@ -930,9 +1248,10 @@ bool APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID lpReserved)
 #endif
 		LoadRealDLL(hModule);
 
-		Init();
-
-		CloseHandle(CreateThread(nullptr, 0, ContinuousThread, nullptr, 0, nullptr));
+		if (Init())
+		{
+			CloseHandle(CreateThread(nullptr, 0, ContinuousThread, nullptr, 0, nullptr));
+		}
 
 		break;
 	case DLL_PROCESS_DETACH:
