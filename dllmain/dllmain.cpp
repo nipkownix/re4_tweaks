937b5a66071bff554c9c471ccaf6ddffba2dd390
emoose <abc@cock.li>
2021-11-11 02:32:23 +0000

    Add hooks to enable debug menu, and `EnableDebugMenu` setting
    Currently is just the default menu, none of the added/reimplemented options from https://github.com/nipkownix/re4_tweaks/issues/8 are added yet.
    Area jump should be fixed though, as long as you have the correct roominfo.dat data it should display area names etc.
    
    Still uses the vanilla bindings too, so use X on controller to choose an option, A to return, will change this later

--- dllmain/dllmain.cpp
+++ dllmain/dllmain.cpp
@@ -58,6 +58,7 @@ bool bUseMemcpy;
 bool bIgnoreFPSWarning;
 bool bWindowBorderless;
 bool bEnableGCBlur;
+bool bEnableDebugMenu;
 
 uintptr_t* ptrResMovAddr;
 uintptr_t* ptrSFDMovAddr;
@@ -451,6 +452,7 @@ void ReadSettings()
 	iWindowPositionY = iniReader.ReadInteger("DISPLAY", "WindowPositionY", -1);
 	bFixQTE = iniReader.ReadBoolean("MISC", "FixQTE", true);
 	bSkipIntroLogos = iniReader.ReadBoolean("MISC", "SkipIntroLogos", false);
+	bEnableDebugMenu = iniReader.ReadBoolean("MISC", "EnableDebugMenu", false);
 	bFixSniperZoom = iniReader.ReadBoolean("MOUSE", "FixSniperZoom", true);
 	bFixRetryLoadMouseSelector = iniReader.ReadBoolean("MOUSE", "FixRetryLoadMouseSelector", true);
 	flip_item_up = iniReader.ReadString("KEYBOARD", "flip_item_up", "HOME");
@@ -476,6 +478,32 @@ void HandleAppID()
 		appid.close();
 	}
 }
+
+// Funcs called by debug menu stuff
+typedef void(__cdecl* DbMenuExec_Fn)();
+typedef void(__cdecl* GameTask_callee_Fn)(void* a1);
+typedef void* (__cdecl* GXLoadImage_Fn)(const char* path);
+typedef void(__cdecl* GX_ScreenDisp_Fn)(void* a1, float a2, float a3, float a4, float a5, float a6, float a7, float a8, float a9, int a10);
+typedef uint32_t(__cdecl* DvdReadN_Fn)(const char* a1, int a2, int a3, int a4, int a5, __int16 a6);
+typedef int(__fastcall* cDvd__ReadCheck_Fn)(void* thisptr, void* unused, uint32_t a2, void* a3, void* a4, void* a5);
+typedef void(*FlagEdit_die_Fn)();
+
+DbMenuExec_Fn DbMenuExec = nullptr;
+GameTask_callee_Fn GameTask_callee_Orig = nullptr;
+GXLoadImage_Fn GXLoadImage = nullptr;
+GX_ScreenDisp_Fn GX_ScreenDisp = nullptr;
+DvdReadN_Fn DvdReadN = nullptr;
+cDvd__ReadCheck_Fn cDvd__ReadCheck = nullptr;
+FlagEdit_die_Fn FlagEdit_die = nullptr;
+
+uintptr_t pG_ptr;
+uintptr_t pSys_ptr;
+
+uint32_t* DbgFontColorArray = nullptr;
+uint32_t* PadButtonStates = nullptr;
+uint32_t* FlagEdit_Backup_pG = nullptr;
+void* cDvd = nullptr;
+uint32_t* roomInfoAddr = nullptr;
 
 void GetPointers()
 {
@@ -640,6 +668,280 @@ void GetPointers()
 
 	varPtr = pattern.count(1).get(0).get<uint32_t>(17);
 	ptr_RenderHeight = (int*)*varPtr;
+
+	// Debug menu stuff
+	pattern = hook::pattern("A1 ? ? ? ? 81 48 ? 00 00 00 80 A1 ? ? ? ? 8B 88 ? ? ? ?");
+	DbMenuExec = (DbMenuExec_Fn)pattern.count(1).get(0).get<uint8_t>(0);
+
+	pattern = hook::pattern("53 E8 ? ? ? ? 8D 4D FC 51 8D 55 F8");
+	fnCallPtr = pattern.count(1).get(0).get<uint8_t>(2);
+	fnCallPtr += sizeof(int32_t) + *(int32_t*)fnCallPtr;
+	GameTask_callee_Orig = (GameTask_callee_Fn)fnCallPtr;
+
+	pattern = hook::pattern("55 8B EC 83 EC ? 56 8B 75 ? 56 E8 ? ? ? ? 50 68 ? ? ? ? 68 00 01 00 00 68 ? ? ? ?");
+	GXLoadImage = (GXLoadImage_Fn)pattern.count(1).get(0).get<uint8_t>(0);
+
+	pattern = hook::pattern("D9 45 ? 03 C1 BA 30 00 00 00");
+	GX_ScreenDisp = (GX_ScreenDisp_Fn)pattern.count(1).get(0).get<uint8_t>(-0x2A);
+
+	pattern = hook::pattern("55 8B EC 56 6A 58 68 ? ? ? ? E8 ? ? ? ? 8B 45 ? 50 68 ? ? ? ?");
+	DvdReadN = (DvdReadN_Fn)pattern.count(1).get(0).get<uint8_t>(0);
+
+	pattern = hook::pattern("55 8B EC 8B 55 ? 81 EC ? ? ? ? 8D 85 ? ? ? ? 50 52 E8 ? ? ? ? 83 F8 01 75");
+	cDvd__ReadCheck = (cDvd__ReadCheck_Fn)pattern.count(1).get(0).get<uint8_t>(0);
+
+	pattern = hook::pattern("A1 ? ? ? ? B9 FF FF FF 7F 21 48 ? A1");
+	FlagEdit_die = (FlagEdit_die_Fn)pattern.count(1).get(0).get<uint8_t>(0);
+	pG_ptr = *(uintptr_t*)pattern.count(1).get(0).get<uint8_t>(1);
+	FlagEdit_Backup_pG = *(uint32_t**)pattern.count(1).get(0).get<uint8_t>(0x14);
+
+	pattern = hook::pattern("FF FF FF FF FF FF 00 00 FF 00");
+	DbgFontColorArray = pattern.count(1).get(0).get<uint32_t>(0);
+
+	pattern = hook::pattern("99 56 8B 35 ? ? ? ? 89 55");
+	varPtr = pattern.count(1).get(0).get<uint32_t>(4);
+	PadButtonStates = (uint32_t*)*varPtr;
+
+	pattern = hook::pattern("6A 40 52 B9 ? ? ? ? E8");
+	varPtr = pattern.count(1).get(0).get<uint32_t>(4);
+	cDvd = (void*)*varPtr;
+
+	pattern = hook::pattern("FF BF FF FF 6A 04 E8 ? ? ? ? 83 C4 ? 85 C0 74 ? 8B 15 ? ? ? ?");
+	varPtr = pattern.count(1).get(0).get<uint32_t>(0x14);
+	roomInfoAddr = (uint32_t*)*varPtr;
+
+	pattern = hook::pattern("00 80 00 00 83 C4 ? E8 ? ? ? ? A1 ? ? ? ?");
+	varPtr = pattern.count(1).get(0).get<uint32_t>(13);
+	pSys_ptr = (uintptr_t)*varPtr;
+
+	// TODO (kinda hard tho)
+	//pFlags_DISP = (uint32_t*)(GameAddress + 0x85A7B8);
+}
+
+// Button definitions used by tool menus
+enum class GamePadButton
+{
+	DPad_Left = 1,
+	DPad_Right = 2,
+	DPad_Down = 4,
+	DPad_Up = 8,
+	RT = 0x10,
+	RB = 0x20,
+	LB = 0x40,
+	LT = 0x80,
+	A = 0x100,
+	B = 0x200,
+	X = 0x400,
+	Y = 0x800,
+	Start = 0x1000,
+	Back = 0x2000,
+	RS = 0x4000,
+	LS = 0x8000,
+	LS_Left = 0x10000,
+	LS_Right = 0x20000,
+	LS_Down = 0x40000,
+	LS_Up = 0x80000,
+	RS_Left = 0x100000,
+	RS_Right = 0x200000,
+	RS_Down = 0x400000,
+	RS_Up = 0x800000,
+};
+
+// recreate gameDebug function from debug EXE, opens tool menu when button combo is pressed
+// 0x63C610 into debug EXE
+uint32_t PrevKeyboardState = 0;
+void __cdecl gameDebug_recreation(void* a1)
+{
+	// Check pressed keys & emulate controller presses for tool-menu
+	uint32_t keyboardState = 0;
+	if (GetAsyncKeyState(VK_HOME))
+		keyboardState |= uint32_t(GamePadButton::LT);
+	if (GetAsyncKeyState(VK_END))
+		keyboardState |= uint32_t(GamePadButton::LS);
+
+	// Only emulate main keys if tool-menu is actually open
+	uint8_t* pG = *(uint8_t**)pG_ptr;
+	bool inDebugMenu = (*(uint32_t*)(pG + 0x60) & 0x80000000) != 0;
+	if (inDebugMenu)
+	{
+		if (GetAsyncKeyState(VK_UP) || GetAsyncKeyState('W'))
+			keyboardState |= uint32_t(GamePadButton::DPad_Up);
+		if (GetAsyncKeyState(VK_DOWN) || GetAsyncKeyState('S'))
+			keyboardState |= uint32_t(GamePadButton::DPad_Down);
+		if (GetAsyncKeyState(VK_LEFT) || GetAsyncKeyState('A'))
+			keyboardState |= uint32_t(GamePadButton::DPad_Left);
+		if (GetAsyncKeyState(VK_RIGHT) || GetAsyncKeyState('D'))
+			keyboardState |= uint32_t(GamePadButton::DPad_Right);
+		if (GetAsyncKeyState(VK_RETURN) || GetAsyncKeyState('F'))
+			keyboardState |= uint32_t(GamePadButton::A);
+		if (GetAsyncKeyState(VK_ESCAPE) || GetAsyncKeyState(VK_BACK))
+			keyboardState |= uint32_t(GamePadButton::B);
+		if (GetAsyncKeyState(VK_DELETE) || GetAsyncKeyState('Q'))
+			keyboardState |= uint32_t(GamePadButton::LB);
+		if (GetAsyncKeyState(VK_NEXT) || GetAsyncKeyState('E')) // pagedown
+			keyboardState |= uint32_t(GamePadButton::RB);
+	}
+
+	if (keyboardState != PrevKeyboardState)
+	{
+		PadButtonStates[0] |= keyboardState;
+
+		// toolmenu funcs check [4], [3] & [1] for some reason
+		PadButtonStates[4] = PadButtonStates[3] = PadButtonStates[1] = PadButtonStates[0];
+
+		PrevKeyboardState = keyboardState;
+	}
+
+	// Check for LT + LS buttons
+	// (make sure they're the only ones pressed - if player opens a UI at same time it'll likely cause a hang...)
+	bool isOnlyDebugComboPressed = PadButtonStates[0] == (uint32_t(GamePadButton::LT) | uint32_t(GamePadButton::LS));
+	if (isOnlyDebugComboPressed)
+	{
+		// Only run DbMenuExec if debug menu isn't open already, otherwise game can hang
+		if (!inDebugMenu)
+			DbMenuExec();
+	}
+
+	GameTask_callee_Orig(a1);
+}
+
+// Funcs hooked for debug menu stuff
+typedef void(*MenuTask_Fn)();
+typedef void(*systemRestartInit_Fn)();
+typedef void* (__cdecl* titleInit_Fn)(void* a1);
+typedef void(__cdecl* FlagEdit_main_Fn)(void* a1);
+
+// restore code that loads dbgfont.tga texture
+// 0x6DB765 into debug EXE
+// DbgFont var doesn't seem to be anywhere in non-dbg EXE, so we'll define our own here
+// the dbg EXE includes code in titleExit to unload DbgFont (at 0x6DD944), but we probably don't need to bother with it
+void* DbgFont = nullptr;
+titleInit_Fn titleInit_Orig = nullptr;
+void* __cdecl titleInit_Hook(void* a1)
+{
+	void* ret = titleInit_Orig(a1);
+
+	DbgFont = GXLoadImage("sv\\dbgfont.tga");
+
+	return ret;
+}
+
+// restore screen-display part of eprintf
+// calls into some GX-layer related function, luckily that func was left (unused) in the final EXE, seems to work fine
+// 0x6DDC30 into debug EXE
+void __cdecl eprintf_ScreenDisp(const char* str, float x, float y, int color)
+{
+	int length = strlen(str);
+
+	double v7 = 0.9900000095367432;
+	double v8 = 0.1428571492433548;
+	double v9 = 0.0625;
+	for (int i = 0; i < length; i++)
+	{
+		char ch = str[i] - 0x20;
+		if (uint8_t(ch) > 0x61)
+			continue;
+
+		float v21 = (float)(ch / 0x10);
+		float v15 = (float)(ch & 0xF);
+
+		float v18 = v15 * v9;
+		float v17 = v21 * v8;
+		float v16 = (v15 + v7) * v9;
+		float v20 = (v21 + v7) * v8;
+
+		GX_ScreenDisp(DbgFont, x + (i * 8), y, 12.0, 18.0, v18, v17, v16, v20, color);
+	}
+}
+char eprintf_buffer[0x80];
+void eprintf_Hook(int a1, int a2, int a3, int a4, char* Format, ...)
+{
+	va_list va;
+	va_start(va, Format);
+
+	vsprintf_s(eprintf_buffer, Format, va);
+
+	eprintf_ScreenDisp(eprintf_buffer, float(a1), float(a2 + 64), DbgFontColorArray[a3]);
+}
+
+MenuTask_Fn MenuTask_Orig = nullptr;
+void MenuTask_Hook()
+{
+	uint8_t* pG = *(uint8_t**)pG_ptr;
+	*(uint32_t*)(pG + 0x60) |= 0x80000000;
+
+	MenuTask_Orig();
+}
+
+uint32_t PrevButtons = 0;
+FlagEdit_main_Fn FlagEdit_main_Orig = nullptr;
+void FlagEdit_main_Hook(void* a1)
+{
+	uint32_t curButtons = PadButtonStates[3];
+	PadButtonStates[3] = 0;
+
+	if (PrevButtons != curButtons)
+		PadButtonStates[3] = curButtons;
+
+	FlagEdit_main_Orig(a1);
+
+	PrevButtons = curButtons;
+	PadButtonStates[3] = curButtons;
+}
+
+// Add code to read debug/roominfo.dat file into roomInfoAddr variable
+// (debug EXE is missing this code, which stops AREA JUMP from working on that!)
+// unfortunately the roominfo.dat provided with RE4 steam is missing the strings for some reason
+// however, roominfo.gc contains the exact strings needed, even at the correct offsets etc!
+// that file is endian-swapped though so can't be used directly...
+// but the strings can be easily copied over by copying 0x2984-EOF over to roominfo.dat :)
+
+// this hook can be removed if roominfo.dat is built into the DLL instead, just need to write roomInfoAddr to point to the data for the file
+// would probably be better than asking users to overwrite their data files too
+
+typedef void(*systemRestartInit_Fn)();
+systemRestartInit_Fn systemRestartInit_Orig = nullptr;
+void systemRestartInit_Hook()
+{
+	systemRestartInit_Orig();
+
+	uint32_t handle = DvdReadN("debug/roomInfo.dat", 0, 0, 0, 0, 5);
+	if (cDvd__ReadCheck(cDvd, 0, handle, 0, 0, roomInfoAddr) < 0)
+		*roomInfoAddr = 0;
+}
+
+void DoDebugMenuHooks()
+{
+	// hook GameTask_callee call to run our gameDebug recreation
+	auto pattern = hook::pattern("53 E8 ? ? ? ? 8D 4D FC 51 8D 55 F8");
+	injector::MakeCALL(pattern.count(1).get(0).get<uint32_t>(1), gameDebug_recreation, true);
+
+	// Remove bzero call that clears flags every frame for some reason
+	pattern = hook::pattern("83 C0 60 6A 10 50 E8");
+	injector::MakeNOP(pattern.count(1).get(0).get<uint32_t>(6), 5, true);
+
+	// Hook MenuTask (task for tool-menu), since it seems the menu can sometimes be shown without the pG+0x60 flag being set
+	// eg. when backing out of area-jump menu
+	// (which could make game hang if this causes us to try opening tool-menu when it's already open...)
+	pattern = hook::pattern("53 57 33 DB 53 E8 ? ? ? ? 6A 01 E8 ? ? ? ? 83 C4");
+	MH_CreateHook(pattern.count(1).get(0).get<uint8_t>(0), MenuTask_Hook, (LPVOID*)&MenuTask_Orig);
+
+	// Hook systemRestartInit so we can make it load in roomInfo.dat
+	pattern = hook::pattern("53 56 E8 ? ? ? ? E8 ? ? ? ? 6A 01 E8 ? ? ? ? A1 ? ? ? ? 83 C0 ? 68");
+	MH_CreateHook(pattern.count(1).get(0).get<uint8_t>(0), systemRestartInit_Hook, (LPVOID*)&systemRestartInit_Orig);
+
+	// Hook eprintf to add screen-drawing to it, like the GC debug has
+	// (required for debug-menu to be able to draw itself)
+	pattern = hook::pattern("55 8B EC 8B 4D ? 8D 45 ? 50 51 68 ? ? ? ? E8 ? ? ? ? 83 C4 ? 5D C3");
+	MH_CreateHook(pattern.count(1).get(0).get<uint8_t>(0), eprintf_Hook, NULL);
+
+	// Hook titleInit to make it load dbgFont for us
+	pattern = hook::pattern("55 8B EC 53 68 ? ? ? ? 68 ? ? ? ? 68 ? ? ? ? 33 DB 53 53 53 53 68 ? ? ? ? C6");
+	MH_CreateHook(pattern.count(1).get(0).get<uint8_t>(0), titleInit_Hook, (LPVOID*)&titleInit_Orig);
+
+	// Hook FlagEdit so we can slow it down by only exposing buttons to it when button state has changed at all
+	pattern = hook::pattern("55 8B EC 56 8B 75 ? 57 33 FF F6 05");
+	MH_CreateHook(pattern.count(1).get(0).get<uint8_t>(0), FlagEdit_main_Hook, (LPVOID*)&FlagEdit_main_Orig);
 }
 
 void HandleLimits()
@@ -1229,6 +1531,11 @@ bool Init()
 		injector::MakeCALL(pattern.get_first(1), Filter01Render_Hook2, true);
 		injector::WriteMemory(pattern.get_first(6), uint8_t(0x58), true); // POP EAX (fixes esp)
 		injector::MakeJMP(pattern.get_first(7), filter01_end, true); // JMP over code that was reimplemented
+	}
+
+	if (bEnableDebugMenu)
+	{
+		DoDebugMenuHooks();
 	}
 
 	// QTE bindings and icons
