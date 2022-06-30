e16537fc5361231394b7f58f4efaaea3c7a1a9fa
nipkow <skidrowa@gmail.com>
2022-06-30 16:22:42 -0300

    Lock cursor inside window when in focus

--- dllmain/WndProcHook.cpp
+++ dllmain/WndProcHook.cpp
@@ -17,6 +17,65 @@ static uint32_t* ptrMouseDeltaX;
 int curPosX;
 int curPosY;
 
+void EnableClipCursor(HWND window) {
+	RECT rect;
+	GetClientRect(window, &rect);
+
+	POINT ul{};
+	ul.x = rect.left;
+	ul.y = rect.top;
+
+	POINT lr{};
+	lr.x = rect.right;
+	lr.y = rect.bottom;
+
+	MapWindowPoints(window, nullptr, &ul, 1);
+	MapWindowPoints(window, nullptr, &lr, 1);
+
+	rect.left = ul.x;
+	rect.top = ul.y;
+
+	rect.right = lr.x;
+	rect.bottom = lr.y;
+
+	if (!ClipCursor(&rect)) {
+		#ifdef VERBOSE
+		con.AddLogChar("Unable to lock cursor");
+		#endif
+		return;
+	}
+
+	#ifdef VERBOSE
+	con.AddLogChar("Cursor locked");
+	#endif
+}
+
+void DisableClipCursor(bool bCenterCursor) {
+	ClipCursor(nullptr);
+
+	// Move cursor to the center of the window
+	if (bCenterCursor)
+	{
+		RECT rect;
+		if (GetWindowRect(hWindow, &rect))
+		{
+			static int width = rect.right - rect.left;
+			static int height = rect.bottom - rect.top;
+
+			SetCursorPos(width / 2, height / 2);
+
+			// Try to show the cursor immediately
+			while (ShowCursor(TRUE) < 0);
+			SetCursor(LoadCursor(NULL, IDC_ARROW));
+			SendMessage(hWindow, WM_SETCURSOR, (WPARAM)hWindow, MAKELPARAM(HTCLIENT, WM_MOUSEMOVE));
+		}
+	}
+
+	#ifdef VERBOSE
+	con.AddLogChar("Cursor released");
+	#endif
+}
+
 // Our new hooked WndProc function
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -37,6 +96,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			break;
 
 		case WM_EXITSIZEMOVE:
+			EnableClipCursor(hWindow);
 			if (cfg.bRememberWindowPos)
 			{
 				// Write new window position
@@ -55,25 +115,26 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			}
 			break;
 
+		case WM_ACTIVATEAPP:
+			if (wParam == WA_ACTIVE)
+			{
+				// Lock cursor inside window
+				EnableClipCursor(hWindow);
+			}
+			break;
+
+		case WM_WINDOWPOSCHANGED:
+			// Lock cursor inside window
+			EnableClipCursor(hWindow);
+			break;
+
 		case WM_KILLFOCUS:
 			// Clear the mouse delta value if we lose focus
 			*(int32_t*)(ptrMouseDeltaX) = 0;
 			_input->clear_mouse_delta();
 
-			// Move cursor to the center of the window
-			RECT rect;
-			if (GetWindowRect(hWindow, &rect))
-			{
-				static int width = rect.right - rect.left;
-				static int height = rect.bottom - rect.top;
-
-				SetCursorPos(width / 2, height / 2);
-
-				// Try to show the cursor immediately
-				while (ShowCursor(TRUE) < 0);
-				SetCursor(LoadCursor(NULL, IDC_ARROW));
-				SendMessage(hWindow, WM_SETCURSOR, (WPARAM)hWindow, MAKELPARAM(HTCLIENT, WM_MOUSEMOVE));
-			}
+			// Unlock cursor
+			DisableClipCursor(true);
 			break;
 
 		case WM_CLOSE:
