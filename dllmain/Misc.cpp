8db27581512c875e5a862d71cbe6c48d93a2f2a4
emoose <abc@cock.li>
2022-08-13 14:49:46 +0100

    SkipIntorLogos: skip titleWarning fade-in

--- dllmain/Misc.cpp
+++ dllmain/Misc.cpp
@@ -1051,6 +1051,15 @@ void Init_Misc()
 		// After that timer, move to stage 0x1E instead of 0x2, making the logos end early
 		injector::WriteMemory(pattern.count(1).get(0).get<uint8_t>(17), uint8_t(0x1E), true);
 
+		// Skip titleWarning fade-in
+		pattern = hook::pattern("0F BE 46 01 83 E8 00 0F");
+		uint8_t* titleWarningCode = pattern.count(1).get(0).get<uint8_t>(0);
+		Patch(titleWarningCode + 0x7, uint16_t(0x9090));
+		Patch(titleWarningCode + 0x9, uint32_t(0x90909090));
+		Patch(titleWarningCode + 0xE, uint16_t(0x9090));
+		Patch(titleWarningCode + 0x11, uint16_t(0x9090));
+		Patch(titleWarningCode + 0x13, uint32_t(0x90909090));
+
 		// Skip logo fade-in
 		pattern = hook::pattern("3B 05 ? ? ? ? 7E ? 3B 05 ? ? ? ? 7D ? 68 00 20 00 00 68 00 10 00 C0");
 		uint8_t* titleLogoCode = pattern.count(1).get(0).get<uint8_t>(0);
