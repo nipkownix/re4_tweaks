7cfcf5c4272f7d68d177661ffe84e9d1e0d2f383
nipkow <skidrowa@gmail.com>
2023-03-21 20:00:27 -0300

    Version bump

--- dllmain/resource.h
+++ dllmain/resource.h
@@ -5,7 +5,7 @@
 #define APP_NAME				"re4_tweaks"
 #define APP_MAJOR				1
 #define APP_MINOR				9
-#define APP_BUILDNUMBER			0
+#define APP_BUILDNUMBER			1
 #define APP_REVISION			0
 #define APP_COMPANYNAME			"nipkow"
 #define APP_DESCRPTION			"Fixes and tweaks for the UHD port of Resident Evil 4"
